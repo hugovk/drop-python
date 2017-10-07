164a37ea777f9ad708e7c8d47e9043fd88063b38
hugovk <hugovk@users.noreply.github.com>
2017-10-07 09:46:19 +0300

    Replace Makefile with scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Prevents script from running if there are any errors
+set -e
+
+# Generate the files
+python generate.py --version 2.6
+python generate.py --version 3.2
+python generate.py --version 3.3
+
+# Make output directory, don't fail if it exists
+mkdir -p build
+
+# Copy to output directory
+cp -R {2.6,3.2,3.3,index.html,wheel.css} build
