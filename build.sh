44c6b7225bab5dfc00d39b84eceeea0dcbed0424
Hugo <hugovk@users.noreply.github.com>
2018-05-05 23:37:25 +0300

    Copy style.css during build

--- build.sh
+++ build.sh
@@ -20,4 +20,4 @@ python template.py --version 2.{0,1,2,3,4,5} 3.{0,1}
 mkdir -p build
 
 # Copy to output directory
-cp -R {2.{0,1,2,3,4,5,6},3.{0,1,2,3},index{,2}.html,results.json,wheel.css} build
+cp -R {2.{0,1,2,3,4,5,6},3.{0,1,2,3},index{,2}.html,results.json,style.css,wheel.css} build
