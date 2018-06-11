b83ed707d2db1d1f2687d9098a8931b033117ece
Hugo <hugovk@users.noreply.github.com>
2018-06-11 12:32:34 +0300

    Explicitly use Python 3

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Prevents script from running if there are any errors
 set -e
 
+# Info
+python3 --version
+
 # Update
 git pull origin master
 
@@ -10,10 +13,10 @@ git pull origin master
 wget https://hugovk.github.io/top-pypi-packages/top-pypi-packages-365-days.min.json -O top-pypi-packages.json
 
 # Generate the files
-python generate.py --version 2.{0,1,2,3,4,5,6} 3.{0,1,2,3}
+python3 generate.py --version 2.{0,1,2,3,4,5,6} 3.{0,1,2,3}
 
 # Create index.html files from the template
-python template.py --version 2.{0,1,2,3,4,5,6} 3.{0,1,2,3}
+python3 template.py --version 2.{0,1,2,3,4,5,6} 3.{0,1,2,3}
 
 # Make output directory, don't fail if it exists
 mkdir -p build
