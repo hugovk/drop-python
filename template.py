00f74f09482686d63e9cb8566abd61dbbc8a5da5
Hugo <hugovk@users.noreply.github.com>
2018-10-08 11:56:14 +0300

    Add whypy3.com as a reason to drop Python 2.7

--- template.py
+++ template.py
@@ -62,6 +62,7 @@ set([1, 2, 3])  # This can be replaced...
         "reasons": """
                     <li><a href="https://pythonclock.org/">Python 2.7 Countdown</a></li>
                     <li><a href="https://python3statement.org/">Sunsetting Python 2 support</a></li>
+                    <li><a href="https://whypy3.com/">Why Python 3?</a></li>
 """,
         "remove": """
                     <p>Follow this guide: <a href="https://python3statement.org/practicalities/">https://python3statement.org/practicalities/</a>
